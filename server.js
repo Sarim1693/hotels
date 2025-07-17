654fd907fd66e925931267ad34f5195468f78b49
Sarim Ashraf <ashrafsraim96@gmail.com>
2025-07-17 20:39:29 +0530

    We have updated our code

--- server.js
+++ server.js
@@ -2,14 +2,29 @@ const express=require('express');
 const app=express();
 const db=require('./db');
 const bodyParser=require('body-parser');
+const passport=require('./auth');           
 app.use(bodyParser.json())
-app.get('/', function(req, res){
-    res.send('Hello Welcome to my restuarent')
-});
+//Middle ware function
+const logRequest=(req, res, next)=>{
+    console.log(`${new Date().toLocaleString()} request made to: ${req.originalUrl}`);
+    next();
+}
+app.use(logRequest);
+
+
+app.use(passport.initialize());
+const localAuthMiddleware=passport.authenticate('local', {session:false});
+app.get('/',  function(req, res){
+    res.send('Welcome to our hotel');
+})
+
+ app.get('/', function(req, res){
+     res.send('Hello Welcome to my restuarent')
+ });
 const menuRoutes=require('./routes/menuRoutes');
-app.use('/Menu', menuRoutes);
 const personRoutes=require('./routes/personRoutes');
-app.use('/person', personRoutes)
+app.use('/Menu', menuRoutes);
+app.use('/person', localAuthMiddleware,personRoutes)
 app.listen(3000, ()=>{
     console.log('Server running at port 3000');
 })
